3b2846148a4d7039d11119539388466555799701
jh508 <Joejohnson2001@hotmail.com>
2023-10-04 13:23:21 +0100

    Added pop-up features for deletion confirmation

--- src/main/resources/static/JavaScript/StudentList.js
+++ src/main/resources/static/JavaScript/StudentList.js
@@ -2,9 +2,10 @@ document.addEventListener("DOMContentLoaded", function() {
     var popup = document.querySelector(".popup");
     var confirmButton = document.getElementById("confirm");
     var cancelButton = document.getElementById("cancel");
-    var deleteButtons = document.querySelectorAll(".delete-btn"); // Use querySelectorAll to select all delete buttons
+    var deleteButtons = document.querySelectorAll(".delete-btn");
     var forms = document.querySelectorAll("form"); // Select all forms on the page
     var selectedIndex;
+
     // Attach an event listener to each form to prevent submission
     forms.forEach(function(form, index) {
         form.addEventListener("submit", function(event) {
@@ -19,24 +20,17 @@ document.addEventListener("DOMContentLoaded", function() {
             selectedIndex = index;
             popup.style.display = "block";
 
-
-            // Handle form submission here or perform any other actions
-            console.log("Form submitted with ID:", postedID);
         });
     });
 
     // Handle confirm button click
     confirmButton.addEventListener("click", function() {
-        // Perform your confirm action here
-        // For example, close the popup
         popup.style.display = "none";
         forms[selectedIndex].submit();
     });
 
     // Handle cancel button click
     cancelButton.addEventListener("click", function() {
-        // Perform your cancel action here
-        // For example, close the popup
         popup.style.display = "none";
         return false;
     });
