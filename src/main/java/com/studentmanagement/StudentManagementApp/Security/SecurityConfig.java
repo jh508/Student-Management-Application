034a8f426a0ad36e11bcfb9a4db562d6b30e7ebb
jh508 <Joejohnson2001@hotmail.com>
2023-10-08 20:12:30 +0100

    Implemented permissions, allowing users with specific roles to view and modify certain parts of the web application

--- src/main/java/com/studentmanagement/StudentManagementApp/Security/SecurityConfig.java
+++ src/main/java/com/studentmanagement/StudentManagementApp/Security/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.studentmanagement.StudentManagementApp.Security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,15 +14,24 @@ public class SecurityConfig {
 
     @Bean
     public InMemoryUserDetailsManager userDetailsManager(){
-        UserDetails john = User.builder().username("admin").password("{noop}test123").roles("ADMIN").build();
-        return new InMemoryUserDetailsManager(john);
+        UserDetails john = User.builder().username("Grandy").password("{noop}g").roles("EMPLOYEE").build();
+        UserDetails joe = User.builder().username("j").password("{noop}j").roles("EMPLOYEE", "ADMIN").build();
+        return new InMemoryUserDetailsManager(john, joe);
     }
+
 
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
-        http.authorizeHttpRequests((configurer) -> configurer.requestMatchers("css/").permitAll().anyRequest().authenticated())
-                .formLogin(form -> form.loginPage("/login")
+        http.authorizeHttpRequests((configurer) -> configurer
+                        .requestMatchers("css/**").permitAll()
+                        .requestMatchers("/student/list").hasRole("EMPLOYEE")
+                        .requestMatchers("/student/**").hasRole("ADMIN")
+                        .requestMatchers(HttpMethod.POST, "/student/**").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.DELETE, "/student/**").hasRole("EMPLOYEE")
+                        .anyRequest().authenticated()
+                )
+                .formLogin(form -> form.loginPage("/login").defaultSuccessUrl("/student/list", true)
                         .loginProcessingUrl("/authenticateuser")
                         .permitAll());
 
