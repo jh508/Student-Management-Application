c51cc699d5380c64a58cdbab8713d428ac5aef88
jh508 <Joejohnson2001@hotmail.com>
2023-10-09 22:43:30 +0100

    Added night mode functionality which saves across sessions using local storage.

--- src/main/resources/static/JavaScript/StudentList.js
+++ src/main/resources/static/JavaScript/StudentList.js
@@ -5,6 +5,36 @@ document.addEventListener("DOMContentLoaded", function() {
     var deleteButtons = document.querySelectorAll(".delete-btn");
     var forms = document.querySelectorAll("#deleteForm"); // Select all forms on the page
     var selectedIndex;
+    const nightModeToggleOff = document.getElementById("nightOn");
+    const nightModeToggleOn = document.getElementById("nightOff");
+    var theme = document.getElementById('cssLink');
+    let nightModeActive = localStorage.getItem("nightMode");
+
+    if(nightModeActive === "true"){
+            theme.href ='/css/studentListNight.css';
+            nightModeToggleOn.style.display = "none";
+            nightModeToggleOff.style.display = "inline";
+        }
+        else{
+            theme.href ='/css/studentList.css';
+            nightModeToggleOn.style.display = "inline";
+            nightModeToggleOff.style.display = "none";
+        }
+
+      nightModeToggleOn.addEventListener("click", function(){
+              nightModeToggleOff.style.display = "inline";
+              nightModeToggleOn.style.display = "none";
+              theme.href ='/css/studentListNight.css';
+              localStorage.setItem("nightMode", "true");
+
+          })
+
+          nightModeToggleOff.addEventListener("click", function(){
+              nightModeToggleOff.style.display = "none";
+              nightModeToggleOn.style.display = "inline";
+              theme.href ='/css/studentList.css';
+              localStorage.setItem("nightMode", "false");
+          })
 
     // Attach an event listener to each form to prevent submission
     forms.forEach(function(form, index) {
