3b33d4117064d5a3e184f29e2cc443126e762b02
jh508 <Joejohnson2001@hotmail.com>
2023-10-10 23:58:45 +0100

    Added logging in via existing users in the database

--- src/main/java/com/studentmanagement/StudentManagementApp/Security/SecurityConfig.java
+++ src/main/java/com/studentmanagement/StudentManagementApp/Security/SecurityConfig.java
@@ -4,19 +4,18 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.LogoutConfigurer;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class SecurityConfig {
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsManager(){
-        UserDetails test = User.builder().username("Grandy").password("{noop}g").roles("EMPLOYEE").build();
-        UserDetails test2 = User.builder().username("j").password("{noop}j").roles("EMPLOYEE", "ADMIN").build();
-        return new InMemoryUserDetailsManager(test, test2);
+    public UserDetailsManager userDetailsManager(DataSource source){
+        return new JdbcUserDetailsManager(source);
     }
 
     @Bean
