7edf6862a1cc91b1a30cdeb382efb4620ab8797e
jh508 <Joejohnson2001@hotmail.com>
2023-10-12 00:33:02 +0100

    Added validation and form data binding to update students.

--- src/main/java/com/studentmanagement/StudentManagementApp/Controllers/StudentController.java
+++ src/main/java/com/studentmanagement/StudentManagementApp/Controllers/StudentController.java
@@ -3,7 +3,6 @@ package com.studentmanagement.StudentManagementApp.Controllers;
 
 import com.studentmanagement.StudentManagementApp.Services.StudentService;
 import com.studentmanagement.StudentManagementApp.Entity.Student;
-import com.studentmanagement.StudentManagementApp.Utilities.StudentControllerUtility;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.propertyeditors.StringTrimmerEditor;
@@ -81,90 +80,35 @@ public class StudentController {
     public String updateStudents(@PathVariable Long id, Model model)
     {
         model.addAttribute("idToUpdate", id);
-        model.addAttribute("firstNameOriginal", studentService.getStudent(id).getFirstName());
         model.addAttribute("firstName", studentService.getStudent(id).getFirstName());
         model.addAttribute("lastName", studentService.getStudent(id).getLastName());
         model.addAttribute("age", studentService.getStudent(id).getAge());
         model.addAttribute("degree", studentService.getStudent(id).getDegree());
+        model.addAttribute("student", studentService.getStudent(id));
         return "updateStudent";
     }
 
-    /**
-     * Handles a POST request to update student information in the database.
-     *
-     * @param id       The user ID.
-     * @param firstName    The user's first name.
-     * @param lastName The user's last name.
-     * @param age      The user's age.
-     * @param degree   The user's degree.
-     * @param button   The button that was pressed (update or cancel).
-     * @return Redirects the user to the student list page.
-     */
+
     @PostMapping("/update")
-    public String updateStudentRequest(@RequestParam("idToUpdate") long id,
-                                       @RequestParam("firstName") String firstName,
-                                       @RequestParam("lastName") String lastName,
-                                       @RequestParam("age") String age,
-                                       @RequestParam("degree") String degree,
+    public String updateStudentRequest(@Valid @ModelAttribute("student") Student student,
+                                       BindingResult bindingResult,
                                        @RequestParam("submitButton") String button,
+                                       @RequestParam("idToUpdate") long id,
                                        Model model) {
 
         if ("cancel".equals(button)) {
             return "redirect:/student/list";
         }
 
-        final String first_Name = firstName.trim();
-        final String last_Name = lastName.trim();
-        final String degree_Name = degree.trim();
-
-        if(first_Name.contains(" ") || first_Name.isBlank() || StudentControllerUtility.containsInvalidCharacters(first_Name)){
-            model.addAttribute("invalidFirstName", "Invalid First Name");
-            model.addAttribute("idToUpdate", id);
-            model.addAttribute("firstNameOriginal", studentService.getStudent(id).getFirstName());
-            model.addAttribute("firstName", first_Name);
-            model.addAttribute("lastName", last_Name);
-            model.addAttribute("age", age);
-            model.addAttribute("degree", degree_Name);
-            return "updateStudent";
+        if(bindingResult.hasErrors()){
+            model.addAttribute("firstName", studentService.getStudent(id).getFirstName());
+            model.addAttribute("lastName", studentService.getStudent(id).getLastName());
+            model.addAttribute("age", studentService.getStudent(id).getAge());
+            model.addAttribute("degree", studentService.getStudent(id).getDegree());
+            return "updateStudent.html";
         }
 
-        if(last_Name.contains(" ") || last_Name.isBlank() || StudentControllerUtility.containsInvalidCharacters(lastName)){
-            model.addAttribute("invalidLastName", "Invalid Last Name");
-            model.addAttribute("idToUpdate", id);
-            model.addAttribute("firstNameOriginal", studentService.getStudent(id).getFirstName());
-            model.addAttribute("firstName", first_Name);
-            model.addAttribute("lastName", last_Name);
-            model.addAttribute("age", age);
-            model.addAttribute("degree", degree_Name);
-            return "updateStudent";
-        }
-
-        if(degree_Name.isBlank() || StudentControllerUtility.containsInvalidCharacters(degree_Name)){
-            model.addAttribute("invalidDegree", "Invalid Degree Name");
-            model.addAttribute("idToUpdate", id);
-            model.addAttribute("firstNameOriginal", studentService.getStudent(id).getFirstName());
-            model.addAttribute("firstName", first_Name);
-            model.addAttribute("lastName", last_Name);
-            model.addAttribute("age", age);
-            model.addAttribute("degree", degree_Name);
-            return "updateStudent";
-        }
-
-        try{
-            int parsedAge = Integer.parseInt(age);
-
-            studentService.updateStudent(id, first_Name, last_Name, parsedAge, degree_Name);
-        }
-        catch (NumberFormatException ex){
-            model.addAttribute("invalidAge", "Age must be a valid integer.");
-            model.addAttribute("idToUpdate", id);
-            model.addAttribute("firstNameOriginal", studentService.getStudent(id).getFirstName());
-            model.addAttribute("firstName", first_Name);
-            model.addAttribute("lastName", last_Name);
-            model.addAttribute("age", age);
-            model.addAttribute("degree", degree_Name);
-            return "updateStudent";
-        }
+        studentService.updateStudent(id, student.getFirstName(), student.getLastName(), student.getAge(), student.getDegree());
 
         return "redirect:/student/list";
     }
